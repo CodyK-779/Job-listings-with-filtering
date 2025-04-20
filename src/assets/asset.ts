24d30e62566ac7ae6bcdf6fd34f2ec2e58a0a9f9
CodyK <Codyk9627@Gmail.com>
2025-04-20 14:59:45 +0630

    Add files via upload
    Initial commit

---
+++ src/assets/asset.ts
@@ -0,0 +1,25 @@
+import photosnap from './images/photosnap.svg';
+import manage from './images/manage.svg';
+import account from './images/account.svg';
+import myHome from './images/myhome.svg';
+import loopStudios from './images/loop-studios.svg';
+import faceit from './images/faceit.svg';
+import shortly from './images/shortly.svg';
+import insure from './images/insure.svg';
+import eyecamCo from './images/eyecam-co.svg';
+import airFilter from './images/the-air-filter-company.svg';
+
+const asset = {
+  photosnap,
+  manage,
+  account,
+  myHome,
+  loopStudios,
+  faceit,
+  shortly,
+  insure,
+  eyecamCo,
+  airFilter
+}
+
+export default asset;
